f909f0dfb7eae436e9bacb6dd82c28a0bc93c7d9
Abhiram172013 <adharapurapuabhiram13@gmail.com>
2025-10-30 18:10:54 +0530

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,10 @@
+def shutdown():
+ shut=str(input("do you want to shut the computer down?(yes/no)"))
+ if shut=="yes":
+    print("shutting down")
+ elif shut=="no":
+    print("abort shut down")
+ else:
+  print("sorry")
+shutdown()
+
