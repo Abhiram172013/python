6a9f461ae67ef3ed5f78051d1a5b78cde61941a1
Abhiram172013 <adharapurapuabhiram13@gmail.com>
2025-11-27 17:44:30 +0530

    Add files via upload

---
+++ operations on tuples.py
@@ -0,0 +1,14 @@
+tuplex=("python",False,30,30.2)
+print(tuplex)
+tuplex=(1,2,3,4,5)
+print(tuplex)
+tuplex=tuplex+(9,)
+print(tuplex)
+tuplex=(5,7,6,7,8)
+print(tuplex.count(7))
+tuplex=(1,3,2,4,5,6,7,8,9)
+slice=tuplex[3:5]
+print(slice)
+slice=tuplex[:6]
+print(slice)
+
