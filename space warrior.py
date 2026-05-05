aa6144fa20a50837da9c3e060e91b66e28ea1517
Abhiram172013 <adharapurapuabhiram13@gmail.com>
2026-05-05 11:47:37 +0530

    Add files via upload

--- space warrior.py
+++ space warrior.py
@@ -52,3 +52,36 @@ over_font=pygame.font.Font('freesansbold.ttf',64)
 def show_score(x,y):
     score=font.render("score:"+str(score_value),True,(255,255,255))
     screen.blit(score,(x,y))
+def game_over_text():
+    over_text=over_font.render('GAME OVER',True,(255,255,255))
+    screen.blit(over_text,(200,250))
+def player(x,y):
+    screen.blit(playerImg,(x,y))
+def enemy(x,y,i):
+    screen.blit(enemyImg[i],(x,y))
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state="fire"      
+    screen.blit(bulletImg,(x+16,y+10))
+def isCollission(enemyX,enemyY,bulletX,bulletY):
+    distance=math.sqrt((enemyX-bulletX)**2+(enemyY-bulletY)**2)
+    return distance < COLLISIONDISTANCE
+running=True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.quit:
+            runing=False
+        if event.type==pygame.KEYDOWN:
+            if event.type==pygame.k_LEFT:
+                playerx_change=-5
+            if event.type==pygame.k_RIGHT:
+                 playerx_change=-5
+            if event.key==pygame.K_SPACE and bullet_state=="ready":
+                bulletX=playerx
+                fire_bullet(bulletX,bulletY)
+        
+
+
+
